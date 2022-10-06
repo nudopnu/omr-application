4642173073626d2e70422f1343b932643990e6ca
Peter Schramm <you@example.com>
2022-10-06 13:05:10 +0200

    Added ux convinience

--- src/Toolbar/ColorLayers/ColorLayers.js
+++ src/Toolbar/ColorLayers/ColorLayers.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { List } from '../../common/List';
 import { getDistinctColors, highlightColor, readAsDataURL } from '../../lib/Image';
 import './ColorLayers.css'
@@ -7,12 +7,26 @@ export function ColorLayers({ canvas, updateImage }) {
 
     const [layers, setLayers] = useState([]);
     const [hasSentImage, setHasSentImage] = useState(false);
+    let toContent = useRef(item => `#${item.r.toString(16).padStart(2, "0")}${item.g.toString(16).padStart(2, "0")}${item.b.toString(16).padStart(2, "0")}`);
 
     useEffect(() => {
         const ctx = canvas.getContext('2d');
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        setLayers(getDistinctColors(imageData.data));
-    }, []);
+        let res = getDistinctColors(imageData.data);
+        let everyGray = true;
+        res.forEach(layer => {
+            if (!(layer.r === layer.g && layer.g === layer.b)) {
+                everyGray = false;
+            }
+        });
+        if (everyGray) {
+            toContent.current = (item) => `#${item.r.toString(16).padStart(2, "0")}${item.g.toString(16).padStart(2, "0")}${item.b.toString(16).padStart(2, "0")} (${item.r})`;
+            res = res.sort((a, b) => a.r - b.r)
+        } else {
+            res = res.sort((a, b) => toContent.current(a).localeCompare(toContent.current(b)))
+        }
+        setLayers(res);
+    }, [canvas]);
 
     async function onLayerSelect(sel) {
         const hex = `#${sel.r.toString(16).padStart(2, "0")}${sel.g.toString(16).padStart(2, "0")}${sel.b.toString(16).padStart(2, "0")}`;
@@ -33,7 +47,7 @@ export function ColorLayers({ canvas, updateImage }) {
             <List
                 items={layers}
                 toKey={item => [item.r, item.g, item.b].join("")}
-                toContent={item => `#${item.r.toString(16).padStart(2, "0")}${item.g.toString(16).padStart(2, "0")}${item.b.toString(16).padStart(2, "0")}`}
+                toContent={toContent.current}
                 onSelect={onLayerSelect}
             />
         </div>
--- src/common/List.js
+++ src/common/List.js
@@ -10,8 +10,50 @@ export function List({ items, onSelect, toKey = (item) => item, toContent = (ite
         onSelect(item);
     }
 
+    let fired = false;
+    let focused = false;
+
+    function onFocus(_event) {
+        console.log("FOCUS");
+        focused = true;
+    }
+    function onBlur(_event) {
+        focused = false;
+        console.log("UNFOCUS");
+    }
+
+    function onKeyDown(event) {
+        event.stopPropagation();
+        if (fired || !focused || !selected) return
+        let curIdx = items.indexOf(item => toKey(item) === selected.current)
+        console.log(items, selected.current, event);
+        switch (event.key) {
+            case 'ArrowUp':
+                if (curIdx > 0) {
+                    setSelected(toKey(items[curIdx - 1]));
+                    onSelect(items[curIdx - 1]);
+                }
+                break;
+            case 'ArrowDown':
+                if (curIdx < items.length - 1) {
+                    setSelected(toKey(items[curIdx + 1]));
+                    onSelect(items[curIdx + 1]);
+                }
+                break;
+            default:
+                break;
+        }
+        fired = true;
+    }
+    function onKeyUp(event) {
+        fired = false;
+    };
+
+    document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
+
     return (
-        <div id="list">
+        <div id="list" onBlur={onBlur} onFocus={onFocus}>
             {items && items.map(item =>
                 <span
                     key={toKey(item)}
