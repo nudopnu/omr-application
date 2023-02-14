8098f6d05c63f934f5ffe27938978e845160e229
Peter Schramm <you@example.com>
2023-02-14 08:56:19 +0100

    Moved background script

--- public/background/predict.js
+++ public/background/predict.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { pythonBackgroundProcess } = require('./background_preload');
 
-pythonBackgroundProcess("predict");
+pythonBackgroundProcess("predict", path.join('..', '..', '..', 'omr-unet'));
